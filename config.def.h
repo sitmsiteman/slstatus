8ceea40438a8eaf351551d35ad92bee942ea80a9
sitmsiteman <sitmsiteman@gmail.com>
2026-04-25 17:51:19 +0900

    add alsa-master

--- config.def.h
+++ config.def.h
@@ -59,6 +59,7 @@ static const char unknown_str[] = "n/a";
  * up                  interface is running            interface name (eth0)
  * uptime              system uptime                   NULL
  * username            username of current user        NULL
+ * alsa_master_vol     ALSA Master device volume       NULL
  * vol_perc            OSS/ALSA volume in percent      mixer file (/dev/mixer)
  *                                                     NULL on OpenBSD/FreeBSD
  * wifi_essid          WiFi ESSID                      interface name (wlan0)
@@ -66,6 +67,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
+	{ alsa_master_vol, "VOL: %s | ", NULL},
 	{ battery_state, "CHR: %s | ", "BAT0"},
 	{ battery_perc, "BAT0: %s%%", "BAT0"},
 	{ battery_remaining, " (%s) | ", "BAT0"},
